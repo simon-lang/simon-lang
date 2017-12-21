aeec113058f4f33405ca3ad7d3015a2fad166a9e
Simon Lang <simon.lang@cambia.org>
2017-12-21 14:41:27 +1000

    Read more functionality

--- src/app.js
+++ src/app.js
@@ -2,21 +2,18 @@ window.addEventListener('load', (event) => {
 
     document.querySelectorAll('.read-more').forEach(d => {
         d.addEventListener('click', function (e) {
-            const el = e.target
             e.preventDefault()
-            el.parentElement.querySelector('.hidden').classList.remove('hidden')
-            // $(el).find('.read-more').text(function (index, text) {
-            //     if (text === 'more info...') {
-            //         return 'collapse'
-            //     } else {
-            //         return 'more info...'
-            //     }
-            // })
-            // $(el).find('img').each(function (el) {
-            //     var src
-            //     src = $(el).attr('lazy-src')
-            //     return $(el).attr('src', src)
-            // })
+            const el = e.target
+            const child = el.parentElement.querySelector('.read-more-content')
+            child.classList.toggle('hidden')
+            const hidden = child.classList.contains('hidden')
+            el.innerText = hidden ? 'more info...' : 'collapse...'
+            child.querySelectorAll('img').forEach(img => {
+                var src = img.getAttribute('lazy-src')
+                if (src) {
+                    img.setAttribute('src', src)
+                }
+            })
         })
     })
 })
